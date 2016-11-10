fb24e5cb8bc8120f20ee0012a8cb8a3733b6c125
Sarah <sarahsilva@live.com>
2016-11-10 17:40:31 -0600

    Added full local authentication functionality.  All pages are inaccessible (minus the login page) unless user is logged in.

--- routes/users.js
+++ routes/users.js
@@ -58,14 +58,42 @@ passport.use(new LocalStrategy(
   function(username, password, done) {
     User.getUserByUsername(username, function(err, user){
       if(err) throw err;
+      if(!user){
+        return done(null, false, {message: 'Unknown User'});
+      }
+      User.comparePassword(password, user.password, function(err, isMatch){
+          if(err) throw err;
+          if(isMatch){
+            return done(null, user);
+          } else {
+            return done(null, false, {message: 'Invalid password'});
+          }
+      });
     });
   }));
 
-app.post('/login',
+
+  passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function(id, done) {
+    User.getUserById(id, function(err, user) {
+      done(err, user);
+    });
+  });
+
+
+router.post('/login',
   passport.authenticate('local', {successRedirect: '/', failureRedirect: '/users/login', failureFlash: true}),
   function(req, res) {
     res.redirect('/');
-
   });
+
+router.get('/logout', function(req, res){
+    req.logout();
+    req.flash('success_msg', 'You are logged out');
+    res.redirect('/users/login');
+});
 
 module.exports = router;
